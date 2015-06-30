af407063e9db8ed8f1c83cee98b13e0736ae64a7
Taketo Takashima <t.taketo1113@gmail.com>
2015-07-01 00:11:30 +0900

    Setup sindan visualization app

--- apps/sindan_visualization/config/environments/production.rb
+++ apps/sindan_visualization/config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Indent html for pretty debugging
+  Slim::Engine.set_options pretty: true
 end
