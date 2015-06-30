83328645a241c888e2b1d4e3daec4de31f4f37f1
Taketo Takashima <t.taketo1113@gmail.com>
2015-07-01 00:11:30 +0900

    Setup sindan visualization app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +39,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # debug Strong Parameters
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
+  # Indent html for pretty debugging
+  Slim::Engine.set_options pretty: true
 end
