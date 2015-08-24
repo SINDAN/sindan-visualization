75aca3479da0b1e9002e2e624ea62723f59f27f0
Taketo Takashima <t.taketo1113@gmail.com>
2015-08-24 18:54:24 +0900

    Added links of log per date

--- apps/sindan_visualization/app/controllers/application_controller.rb
+++ apps/sindan_visualization/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_diagnosis_log_dates
+
+  def set_diagnosis_log_dates
+    @diagnosis_log_dates = DiagnosisLog.all.map{ |d| d.occurred_at.to_date.to_s }.uniq
+  end
 end
