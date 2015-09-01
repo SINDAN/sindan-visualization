9c86e2c4425a410d31ebf4c5dbf9dbbb8133f50b
Taketo Takashima <t.taketo1113@gmail.com>
2015-09-02 01:33:30 +0900

    Changed order of log campaign list

--- app/models/log_campaign.rb
+++ app/models/log_campaign.rb
@@ -1,6 +1,8 @@
 class LogCampaign < ActiveRecord::Base
   has_many :diagnosis_logs, foreign_key: :log_campaign_uuid, primary_key: :log_campaign_uuid
 
+  default_scope { order(occurred_at: :desc) }
+
   validates_uniqueness_of :log_campaign_uuid,
                           if: Proc.new { |record| !record.log_campaign_uuid.blank? }
 end
