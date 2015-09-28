825eac5f8e69e693489fb3ef41c72300c32e7a0c
Taketo Takashima <t.taketo1113@gmail.com>
2015-09-28 19:40:08 +0900

    Fixed spec with user auth

--- apps/sindan_visualization/spec/factories/users.rb
+++ apps/sindan_visualization/spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, class: User do
     login "name"
     email "user@example.com"
     password "user@example.com"
   end
 
+  factory :login_user, class: User do
+    login "login_user"
+    email "login_user@example.com"
+    password "login_user@example.com"
+  end
 end
--- apps/sindan_visualization/spec/requests/diagnosis_logs_spec.rb
+++ apps/sindan_visualization/spec/requests/diagnosis_logs_spec.rb
@@ -5,36 +5,75 @@ RSpec.describe "DiagnosisLogs", type: :request do
     @diagnosis_log = FactoryGirl.create(:diagnosis_log)
   end
 
-  describe "GET /diagnosis_logs" do
-    it "works!" do
-      get diagnosis_logs_path
-      expect(response).to have_http_status(200)
+  context "for authenticated users" do
+    request_login_user
+
+    describe "GET /diagnosis_logs" do
+      it "works!" do
+        get diagnosis_logs_path
+        expect(response).to have_http_status(200)
+      end
+
+      it "works! with date params" do
+        get diagnosis_logs_path(date: '20150731')
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it "works! with date params" do
-      get diagnosis_logs_path(date: '20150731')
-      expect(response).to have_http_status(200)
+    describe "GET /diagnosis_logs/:id" do
+      it "works!" do
+        get diagnosis_log_path(@diagnosis_log)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /diagnosis_logs/new" do
+      it "works!" do
+        get new_diagnosis_log_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /diagnosis_logs/:id/edit" do
+      it "works!" do
+        get edit_diagnosis_log_path(@diagnosis_log)
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
-  describe "GET /diagnosis_logs/:id" do
-    it "works!" do
-      get diagnosis_log_path(@diagnosis_log)
-      expect(response).to have_http_status(200)
-    end
-  end
+  context "for no authenticated users" do
+    describe "GET /diagnosis_logs" do
+      it "redirects to login page" do
+        get diagnosis_logs_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
 
-  describe "GET /diagnosis_logs/new" do
-    it "works!" do
-      get new_diagnosis_log_path
-      expect(response).to have_http_status(200)
+      it "redirects to login page with date params" do
+        get diagnosis_logs_path(date: '20150731')
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
-  end
 
-  describe "GET /diagnosis_logs/:id/edit" do
-    it "works!" do
-      get edit_diagnosis_log_path(@diagnosis_log)
-      expect(response).to have_http_status(200)
+    describe "GET /diagnosis_logs/:id" do
+      it "redirects to login page" do
+        get diagnosis_log_path(@diagnosis_log)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe "GET /diagnosis_logs/new" do
+      it "redirects to login page" do
+        get new_diagnosis_log_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe "GET /diagnosis_logs/:id/edit" do
+      it "redirects to login page" do
+        get edit_diagnosis_log_path(@diagnosis_log)
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
