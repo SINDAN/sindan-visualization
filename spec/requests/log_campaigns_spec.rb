ec528983eea3c9452f7f64b6e9e597959acca190
Taketo Takashima <t.taketo1113@gmail.com>
2015-09-28 19:40:08 +0900

    Fixed spec with user auth

--- spec/requests/log_campaigns_spec.rb
+++ spec/requests/log_campaigns_spec.rb
@@ -5,31 +5,65 @@ RSpec.describe "LogCampaigns", type: :request do
     @log_campaign = FactoryGirl.create(:log_campaign)
   end
 
-  describe "GET /log_campaigns" do
-    it "works!" do
-      get log_campaigns_path
-      expect(response).to have_http_status(200)
+  context "for authenticated users" do
+    request_login_user
+
+    describe "GET /log_campaigns" do
+      it "works!" do
+        get log_campaigns_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /log_campaigns/:id" do
+      it "works!" do
+        get log_campaign_path(@log_campaign)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /log_campaigns/new" do
+      it "works!" do
+        get new_log_campaign_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /log_campaigns/:id/edit" do
+      it "works!" do
+        get edit_log_campaign_path(@log_campaign)
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
-  describe "GET /log_campaigns/:id" do
-    it "works!" do
-      get log_campaign_path(@log_campaign)
-      expect(response).to have_http_status(200)
+  context "for no authenticated users" do
+    describe "GET /log_campaigns" do
+      it "redirects to login page" do
+        get log_campaigns_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
-  end
 
-  describe "GET /log_campaigns/new" do
-    it "works!" do
-      get new_log_campaign_path
-      expect(response).to have_http_status(200)
+    describe "GET /log_campaigns/:id" do
+      it "redirects to login page" do
+        get log_campaign_path(@log_campaign)
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
-  end
 
-  describe "GET /log_campaigns/:id/edit" do
-    it "works!" do
-      get edit_log_campaign_path(@log_campaign)
-      expect(response).to have_http_status(200)
+    describe "GET /log_campaigns/new" do
+      it "redirects to login page" do
+        get new_log_campaign_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe "GET /log_campaigns/:id/edit" do
+      it "redirects to login page" do
+        get edit_log_campaign_path(@log_campaign)
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
