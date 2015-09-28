5d205e1ef92c1e6eafc2b96cff1863510812d9eb
Taketo Takashima <t.taketo1113@gmail.com>
2015-09-28 18:57:10 +0900

    Changed user login column

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before(:each) do
     @user = User.new(
-      name: 'name',
+      login: 'name',
       email: 'user@example.com',
       password: 'user@example.com',
     )
@@ -11,5 +11,20 @@ RSpec.describe User, type: :model do
 
   it "is valid with valid attributes" do
     expect(@user).to be_valid
+  end
+
+  it "is not valid without login" do
+    @user.login = nil
+
+    expect(@user).not_to be_valid
+  end
+
+  it "is not valid with same value of login" do
+    @user.login = 'name'
+    @user.save
+
+    @user2 = User.new(login: 'name')
+
+    expect(@user2).not_to be_valid
   end
 end
