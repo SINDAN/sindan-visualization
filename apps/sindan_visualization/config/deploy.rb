d0e17276cf93f8f933cadbb43c36f6a3580428be
Taketo Takashima <t.taketo1113@gmail.com>
2016-08-14 16:59:12 +0900

    Updated ruby version for deploy script

--- apps/sindan_visualization/config/deploy.rb
+++ apps/sindan_visualization/config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/www/sindan-production'
 
 # Set the ruby version
 set :rbenv_type, :system
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.3.1'
 
 # server alias
 set :sindan, "fluentd.c.u-tokyo.ac.jp"
