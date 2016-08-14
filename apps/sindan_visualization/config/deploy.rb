1d295971854b7fb2586963264f3763f9cfd7faf7
Taketo Takashima <t.taketo1113@gmail.com>
2016-08-14 18:08:13 +0900

    Removed unecessary settings for deploy script

--- apps/sindan_visualization/config/deploy.rb
+++ apps/sindan_visualization/config/deploy.rb
@@ -48,7 +48,7 @@ set :log_level, :debug # :debug or :info
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
