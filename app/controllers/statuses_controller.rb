1a3628685bc301ac9e8af14e4903bce32ed8103d
Taketo Takashima <t.taketo1113@gmail.com>
2016-09-08 22:11:15 +0900

    Fixed to time range for status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -8,12 +8,13 @@ class StatusesController < ApplicationController
 #    @ssid_list = LogCampaign.uniq.pluck(:ssid)
 
     term = 10
+    count = 12
     time = Time.zone.now
-    @time_list = (11 * term).step(0, -1 * term).map { |i| time - i.minutes }
+    @time_list = ((count - 1) * term).step(0, -1 * term).map { |i| time - i.minutes }
     @layers = DiagnosisLog.layer_defs.keys.reverse
 
     # TODO: get log per ssid
-    @target_logs = DiagnosisLog.occurred_after(time - 1.hours)
+    @target_logs = DiagnosisLog.occurred_after(time - (term * count).minutes)
 
     @diagnosis_logs = @target_logs.fail
   end
