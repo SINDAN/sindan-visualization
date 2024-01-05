5a99c7144832754332fad328d6fc8e262d5101e1
Taketo Takashima <t.taketo1113@gmail.com>
2024-01-06 00:20:30 +0900

    Updated config for rails 7.1

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
   resources :ignore_error_results
 
   get 'about', to: 'static_pages#about'
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 end
